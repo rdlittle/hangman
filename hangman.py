bafe36e8408a561bd50010c05b6c40ecd0a2d233
rlittle <rlittle@littleacres.org>
2020-05-07 08:06:26 -0400

    Moved themes dict to its own module

--- hangman.py
+++ hangman.py
@@ -4,27 +4,7 @@ import random
 from images import Picture
 import pdb
 import sys
-
-themes = {
-    'sea': {
-        'dark': '#00ffaa',
-        'medium': '#79d1be',
-        'light': '#569587',
-        'offset': '#36da96'
-    },
-    'sky': {
-        'dark': '#00599d',
-        'medium': '#008cf7',
-        'light': '#aaffff',
-        'offset': '#91dada'
-    },
-    'earth': {
-        'dark': '#685038',
-        'medium': '#b48b61',
-        'light': '#ffc58a',
-        'offset': '#c39669'
-    }
-}
+from themes import themes
 
 theme = themes['sea']
 
---
+++ themes.py
@@ -0,0 +1,20 @@
+themes = {
+    'sea': {
+        'dark': '#00ffaa',
+        'medium': '#79d1be',
+        'light': '#569587',
+        'offset': '#36da96'
+    },
+    'sky': {
+        'dark': '#00599d',
+        'medium': '#008cf7',
+        'light': '#aaffff',
+        'offset': '#91dada'
+    },
+    'earth': {
+        'dark': '#685038',
+        'medium': '#b48b61',
+        'light': '#ffc58a',
+        'offset': '#c39669'
+    }
+}
